3390571089be857a7ae6a7bab9815fdcb6d257f7
Chris Pachomski <cpachomski@gmail.com>
2015-03-06 15:08:31 -0500

    create snippet model with writer methods

--- rebase.js
+++ rebase.js
@@ -1,19 +1,58 @@
+
+
+//MODELS
+
+    //SNIPPET
+
+    //js things to note for functions
+      //currentTime = start differential of the snippet relative to it's own zero
+        //use for ST and END
+
+
+
+
+
+
+
+
+
+
+      // $('#audio').bind('canplay', function() {
+      //   this.currentTime = 29; // jumps to 29th secs
+      // });
+
+  //CONTROLLER
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+      function Snippet(url, title){
+        this.url = url;
+        this.title = title;
+        this.cueIn = 0;
+        this.cueOut = 0;
+        this.start_time = 0;
+        this.end_time = 0;
+      }
+
+      Snippet.prototype.set_start = function(start_time){
+        this.start_time = start_time;
+      };
+
+      Snippet.prototype.set_end = function(end_time){
+        this.end_time = end_time;
+      };
+
+      Snippet.prototype.set_cue_in = function(cue_in_time){
+        this.cue_in = cue_in_time;
+      };
+
+      Snippet.prototype.set_cue_out = function(cue_out_time){
+        this.cue_out = cue_out_time;
+      };
+
+
 }
 
 if (Meteor.isServer) {
