4e9a1adceedeb708f08eac33a77c8bd369b51e1b
Chris Pachomski <cpachomski@gmail.com>
2015-03-06 12:20:18 -0500

    add test sample urls to view

--- rebase.js
+++ rebase.js
@@ -2,18 +2,18 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  // Template.hello.helpers({
+  //   counter: function () {
+  //     return Session.get('counter');
+  //   }
+  // });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  // Template.hello.events({
+  //   'click button': function () {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 1);
+  //   }
+  // });
 }
 
 if (Meteor.isServer) {
@@ -21,3 +21,19 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+//create div with html5 audio tag inside
+// add url to html 5 audio tag
+// add start time and end time to audio tag
+// play multiple urls at once starting at the same time
+// play multiple urls starting at different times
+
+
+
+
+
+//mediaController will hold all the tracks for stump/Session
+
+
+
+
