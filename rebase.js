8767ce899a5463f6c400b6d53792eb829f16f829
cpachomski <cpachomski@gmail.com>
2015-03-07 17:28:13 -0500

    move templates to folders and add .js files

--- rebase.js
+++ rebase.js
@@ -11,43 +11,7 @@ Snippets = new Mongo.Collection("snippets");
 
 if (Meteor.isClient) {
 
-  //TEST
-    Template.session.events({
 
-      "submit .new-cueIn": function(event){
-        event.preventDefault();
-        var cue = event.target.text.value;
-
-        Snippets.update({"_id" : "RZfK3WvWLqcWpdcho"},
-          {$set:{cueIn: cue}}
-          );
-
-        event.target.text.value = "";
-
-      },
-
-       "submit .new-cueOut": function(event){
-        event.preventDefault();
-        var cue = event.target.text.value;
-
-
-        Snippets.update({"_id" : "RZfK3WvWLqcWpdcho"},
-          {$set:{cueOut: cue}}
-          );
-        event.target.text.value = "";
-
-      }
-    });
-
-    Template.session.helpers({
-      cueIn: function () {
-        return Snippets.findOne({"_id" : "RZfK3WvWLqcWpdcho"}).cueIn;
-      },
-
-      cueOut: function () {
-        return Snippets.findOne({"_id" : "RZfK3WvWLqcWpdcho"}).cueOut;
-      }
-    });
 
   }
 
