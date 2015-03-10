0a2f67a3167b185fd5fcb9c2d2b39f664e47a56a
James Parseghian <jjparseghian@gmail.com>
2015-03-10 11:57:07 -0400

    Add history template helpers and event listeners

---
+++ client/templates/history.js
@@ -0,0 +1,33 @@
+var selectedTimeInHistory
+
+Template.history_selector.helpers({
+  ts_from_history: function () {
+    var timestamps = Histories.find().fetch()
+    return timestamps
+  }
+});
+
+Template.history_selector.events({
+  'change #history-selector': function (event) {
+    event.preventDefault();
+    console.log("target ", event.target);
+    var select = event.target;
+    console.log("target value ", event.target.value);
+    Session.set('snippet_list', event.target.value)
+    //
+    //on submit find the snippets in the history collection and display them on the page
+    // selectedTimeInHistory = // event target value
+  }
+});
+
+Template.snippet_history.helpers({
+  snippets_from_history: function () {
+    var oldSnippets = Histories.find({ts: selectedTimeInHistory})
+  }
+});
+
+// Template.history_temp_container.helpers({
+//   whichOne: function () {
+//     return Session.get("hist") ? ""
+//   }
+// });
